af446b55a5c30d3ec6a9a192bf3eecb830782924
wilsalr <103476718+wilsalr@users.noreply.github.com>
2022-04-18 19:24:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello World!\n')
-})
+    res.send('Nama : Ryu Wilsal\n')
+    res.send('NIM : 52019033\n')
+    res.send('Jurusan : TI-2019\n')
+})))
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
