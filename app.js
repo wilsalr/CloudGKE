828188b1924298245570020d7c541f7539cd58a8
wilsalr <103476718+wilsalr@users.noreply.github.com>
2022-04-18 15:41:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Ryu Wilsa - 52019033 - Teknik Informatika!\n')
+    res.send('Ryu Wilsal - 52019033 - Teknik Informatika!\n')
 })
 
 app.listen(PORT, HOST)
